f42bd1b09c2961af4d1a531fe66ee01be0972332
Douglas Vilela <douglasvilelaa@gmail.com>
2021-03-11 16:45:55 -0300

    atualização aplicativo

--- app/src/main/java/com/douglasvilella/biscoitodasorte/MainActivity.kt
+++ app/src/main/java/com/douglasvilella/biscoitodasorte/MainActivity.kt
@@ -30,7 +30,17 @@ class MainActivity : AppCompatActivity() {
             "É mais fácil conseguir o perdão do que a permissão",
             "A maior barreira para o sucesso é o medo do fracasso.",
             "O insucesso é apenas uma oportunidade para recomeçar de novo com mais experiência.",
-            "Motivação não é sinónimo de transformação, mas um passo em sua direção."
+            "Motivação não é sinónimo de transformação, mas um passo em sua direção.",
+            "Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você",
+            "A maior de todas as torres começa no solo.",
+            "A juventude não é uma época da vida, é um estado de espírito.",
+            "Aquele que se importa com o sentimento dos outros, não é um tolo.",
+            "O riso é a menor distância entre duas pessoas.",
+            "Surpreender e ser surpreendido é o segredo do amor.",
+            "Espere pelo mais sábio dos conselhos: o tempo.",
+            "Vencer é 90 por cento suor e 40 por cento técnica.",
+            "Deixe de lado as preocupações e seja feliz.",
+            "Coragem é a resistência ao medo, domínio do medo, e não a ausência do medo."
 
 
         )
